5d81c17daa3455002c227fca43664d50555bdf32
Azusa <akiriin0@gmail.com>
2021-10-01 14:29:15 -0400

    Try fix action fail

--- ShapeTextureProgram.cpp
+++ ShapeTextureProgram.cpp
@@ -3,8 +3,15 @@
 #include "gl_compile_program.hpp"
 #include "gl_errors.hpp"
 
+#ifdef _MSC_VER
+#define DEBUG_BREAK __debugbreak()
+#elif __APPLE__
+#define DEBUG_BREAK __builtin_trap()
+#else
+#define DEBUG_BREAK raise(SIGTRAP)
+#endif
 
-#define ASSERT(x) if (!(x)) __debugbreak();
+#define ASSERT(x) if (!(x)) DEBUG_BREAK;
 #define GLCall(x) GLClearError();\
 	x;\
 	ASSERT(GLLogCall(#x, __FILE__, __LINE__))
